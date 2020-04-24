52e4d818dce602c08a738d25a4bf78dfc527f2d7
Guilherme Kammsetzer <guilherme@intruder.network>
2020-04-24 16:56:59 -0300

    Initial commit

--- src/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Sum } from '.'
-
-describe('sum', () => {
-  it('should sum two numbers', () => {
-    expect(Sum(2, 2)).toBe(4)
-  })
-})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,65 @@
-export const Sum = (x: number, y: number) => x + y
+import express from 'express'
+import bodyParser from 'body-parser'
+import crypto from 'crypto'
+import { readdirSync } from 'fs'
+import { execSync } from 'child_process'
+import repositories from '../repositories.json'
+
+const app = express()
+
+const main = () => {
+  app.use(bodyParser.raw({ type: '*/*' }))
+
+  app.get('/', (_, res) => res.send('Armandin pilantra'))
+
+  app.post('/', (req, res) => {
+    try {
+      const sigHeader = req.headers['x-gogs-signature']
+
+      if (!sigHeader || !req.body) {
+        res.send('Armandin pilantra')
+        return
+      }
+
+      const sig = crypto
+        .createHmac('sha256', 'armandin-da-o-boga')
+        .update(req.body)
+        .digest('hex')
+
+      if (sig !== sigHeader) {
+        res.send('Armandin pilantra')
+        return
+      }
+
+      const body = JSON.parse(req.body.toString())
+
+      const files = readdirSync(repositories.basePath)
+
+      if (files.indexOf(body.repository.name) < 0) {
+        res.send('Armandin pilantra')
+        return
+      }
+
+      const repo = repositories[body.repository.name]
+
+      if (!repo || !repo.commands.length) {
+        res.send('Armandin pilantra')
+        return
+      }
+
+      process.chdir(repositories.basePath + body.repository.name)
+
+      repo.commands.map(execSync)
+    } catch (err) {
+      console.error(err)
+      res.send('Armandin pilantra')
+      return
+    }
+
+    res.send('ok')
+  })
+}
+
+main()
+
+app.listen(8888)
